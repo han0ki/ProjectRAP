a356e9a7ff539cb49ce53bae18bfe765a63cf3f5
SYNTHCAKE <43035326+SYNTHCAKE@users.noreply.github.com>
2019-04-09 00:00:46 +0300

    Add files via upload

---
+++ adr.php
@@ -0,0 +1,5 @@
+<?php
+$host = '127.0.0.1:3306'; 
+$database = 'tab1'; 
+$user = 'root'; 
+$password = '1'; 
---
+++ controller.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'adr.php'; 
+ 
+$link = mysqli_connect($host, $user, $password, $database) 
+    or die("Ошибка " . mysqli_error($link));
+ 
+if (!$link) {
+die("Connection failed: " . mysqli_connect_error());
+}
+ 
+$name = strip_tags(trim($_POST['name']));
+$mail = strip_tags(trim($_POST['mail']));
+$message = strip_tags(trim($_POST['message']));
+$sql = "INSERT INTO tablica (name, mail, message) VALUES ('$name', '$mail', '$message')";
+if (mysqli_query($link, $sql)) {
+
+} else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($link);
+}
+mysqli_close($link);
+
+?>
