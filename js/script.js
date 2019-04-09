2a8193530acb04651b6236865bb1c80990eeab0e
han0ki <43035482+han0ki@users.noreply.github.com>
2019-04-09 22:24:40 +0300

    + 5 stranic, +fixed, +adap

--- js/script.js
+++ js/script.js
@@ -11,3 +11,15 @@
     $('.slider').slider();
   });
 
+$('a[href^="#glav"]').bind('click.smoothscroll',function (e) {
+e.preventDefault();
+
+var target = this.hash,
+$target = $(target);
+
+$('html, body').stop().animate({
+'scrollTop': $target.offset().top
+}, 1500, 'swing', function () {
+window.location.hash = target;
+});
+});
